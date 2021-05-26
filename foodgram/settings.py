97d1eb06833d981f8ea12c21b9f6a4bdb61105ce
Aleksei Briushinin <megalaren@mail.ru>
2021-05-26 18:31:13 +0300

    Добавлены модели в recipes

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -13,17 +13,20 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'users.User'
 
 INSTALLED_APPS = [
     'about.apps.AboutConfig',
     'api.apps.ApiConfig',
     'recipes.apps.RecipesConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sorl.thumbnail',
 ]
 
 MIDDLEWARE = [
@@ -100,6 +103,9 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,104 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Color(models.TextChoices):
+    ORANGE = 'orange', 'Оранжевый'
+    GREEN = 'green', 'Зеленый'
+    PURPLE = 'purple', 'Фиолетовый'
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        verbose_name='Имя тега',
+        max_length=50,
+    )
+    slug = models.SlugField(
+        verbose_name='Slug',
+        max_length=50,
+        unique=True,
+    )
+    # get_color_display() - получить удобно читаемый цвет
+    color = models.CharField(
+        verbose_name='Цвет',
+        max_length=20,
+        choices=Color.choices,
+        default=Color.GREEN,
+    )
+
+
+class Ingredient(models.Model):
+    title = models.CharField(
+        verbose_name='Название ингредиента',
+        max_length=50,
+    )
+    unit = models.CharField(
+        verbose_name='Единица измерения',
+        max_length=50,
+    )
+
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор рецепта',
+        on_delete=models.CASCADE,
+        related_name='recipes',
+    )
+    title = models.CharField(
+        verbose_name='Название рецепта',
+        max_length=100,
+    )
+    description = models.TextField(
+        verbose_name='Описание рецепта',
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True,
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Теги',
+        blank=True,
+        related_name='recipes',
+    )
+    image = models.ImageField(
+        upload_to='recipes/',
+        help_text='Можете добавить картинку',
+        blank=True,
+        null=True
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        verbose_name='Время приготовления в минутах',
+        validators=[MinValueValidator(0)],
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='RecipeIngredient',
+        verbose_name='Ингредиент',
+    )
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        verbose_name='Рецепт',
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        verbose_name='Ингредиент'
+    )
+    quantity = models.DecimalField(
+        verbose_name='Количество',
+        max_digits=6,
+        decimal_places=1,
+        null=True,
+        blank=True,
+        validators=[
+            MinValueValidator(0)]
+    )
