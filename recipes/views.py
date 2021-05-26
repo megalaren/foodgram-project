7c51687139125ef66c07862f9b2f8d65b1b3bf40
Aleksei Briushinin <megalaren@mail.ru>
2021-05-26 18:31:13 +0300

    Добавлены модели в recipes

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 
 
+User = get_user_model()
+
+
 def index(request):
+    user = request.user
     return render(request, 'recipes/indexAuth.html')
