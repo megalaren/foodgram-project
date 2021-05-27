bf270b4938da3e73af918223846b9e9f1ed44497
Aleksei Briushinin <megalaren@mail.ru>
2021-05-27 17:54:24 +0300

    Отредактирован шаблон и вью-функция главной страницы

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,14 @@
 from django.contrib.auth import get_user_model
 from django.shortcuts import render
 
+from .models import Recipe
+
 
 User = get_user_model()
 
 
 def index(request):
-    user = request.user
-    return render(request, 'recipes/indexAuth.html')
+    recipes = Recipe.objects.select_related('author')
+    return render(request, 'recipes/index.html', {
+        'recipes': recipes,
+    })
