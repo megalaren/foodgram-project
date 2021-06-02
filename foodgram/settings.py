2a245056e7fc9e11984e4625b0badea6233af071
Aleksei Briushinin <megalaren@mail.ru>
2021-06-02 14:33:13 +0300

    Добавлены модели в api

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'sorl.thumbnail',
 ]
 
@@ -90,7 +91,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
