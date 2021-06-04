ac7178bedec44df22e2d49b0ee2eb20e7764ef73
Aleksei Briushinin <megalaren@mail.ru>
2021-06-04 12:07:51 +0300

    Сделано добавление в избранное

--- api/views.py
+++ api/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import mixins, status, viewsets
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Favorite, Follow, Purchase
+from .serializers import FavoriteSerializer
+
+User = get_user_model()
+
+UNKNOWN_ERROR = 'Произошла неизвестная ошибка'
+
+
+class FavoriteViewSet(mixins.CreateModelMixin, mixins.DestroyModelMixin,
+                      viewsets.GenericViewSet):
+    lookup_field = 'recipe'
+    serializer_class = FavoriteSerializer
+
+    def get_queryset(self):
+        return Favorite.objects.filter(user=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(
+                {'success': False, 'detail': serializer.errors},
+                status=status.HTTP_400_BAD_REQUEST, )
+        if not Favorite.objects.filter(
+                user=request.user,
+                recipe=serializer.validated_data['recipe']).exists():
+            serializer.save(user=request.user)
+        return Response({'success': True}, status=status.HTTP_200_OK)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance and not instance.delete():
+            return Response(
+                {'success': False, 'detail': UNKNOWN_ERROR},
+                status=status.HTTP_400_BAD_REQUEST,)
+        return Response({'success': True}, status=status.HTTP_200_OK)
--- foodgram/settings.py
+++ foodgram/settings.py
@@ -92,6 +92,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',  # только для тестирования
+        'rest_framework.authentication.SessionAuthentication',
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
