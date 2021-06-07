3924ea81a6f39a44899ee26518bb24dcf011c4a8
Aleksei Briushinin <megalaren@mail.ru>
2021-06-07 13:16:29 +0300

    Добавлена страница избранное, переработана singlePage

--- recipes/views.py
+++ recipes/views.py
@@ -9,30 +9,47 @@ from api.models import Favorite, Follow, Purchase
 User = get_user_model()
 
 
+def get_recipes_for_index(recipes, user):
+    """Возвращает рецепты с полями is_favorite и is_purchase."""
+    favorites_recipes = Favorite.objects.filter(
+        user=user,
+        recipe=OuterRef('pk')
+    )
+    purchases_recipes = Purchase.objects.filter(
+        user=user,
+        recipe=OuterRef('pk')
+    )
+    recipes = recipes.annotate(
+        is_favorite=Exists(favorites_recipes)).annotate(
+        is_purchase=Exists(purchases_recipes)
+    )
+    return recipes
+
+
 def index(request):
     recipes = Recipe.objects.select_related('author')
     user = request.user
-    purchases_count = 0
     if user.is_authenticated:
-        purchases_count = user.purchases.count()
-        favorites_recipes = Favorite.objects.filter(
-            user=user,
-            recipe=OuterRef('pk')
-        )
-        purchases_recipes = Purchase.objects.filter(
-            user=request.user,
-            recipe=OuterRef('pk')
-        )
-        recipes = recipes.annotate(
-            is_favorite=Exists(favorites_recipes)).annotate(
-            is_purchase=Exists(purchases_recipes))
-
+        recipes = get_recipes_for_index(recipes, user)
     all_tags = Tag.objects.all()
 
     return render(request, 'recipes/index.html', {
         'recipes': recipes,
         'all_tags': all_tags,
-        'purchases_count': purchases_count
+    })
+
+
+@login_required
+def favorite(request):
+    user = request.user
+    recipes = Recipe.objects.filter(
+        favorites__user=user).select_related('author')
+    recipes = get_recipes_for_index(recipes, user)
+    all_tags = Tag.objects.all()
+
+    return render(request, 'recipes/favorite.html', {
+        'recipes': recipes,
+        'all_tags': all_tags,
     })
 
 
@@ -41,13 +58,11 @@ def follow_index(request):
     authors = User.objects.filter(
         following__user=request.user).prefetch_related('recipes').annotate(
         recipes_count=Count('recipes'))
-    purchases_count = request.user.purchases.count()
     all_tags = Tag.objects.all()
 
     return render(request, 'recipes/myFollow.html', {
         'authors': authors,
         'all_tags': all_tags,
-        'purchases_count': purchases_count
     })
 
 
@@ -56,39 +71,44 @@ def profile(request, username):
     recipes = author.recipes.all()
     user = request.user
     is_follow = False
-    purchases_count = 0
     if user.is_authenticated:
-        purchases_count = user.purchases.count()
-        favorites_recipes = Favorite.objects.filter(user=user,
-            recipe=OuterRef('pk'))
-        purchases_recipes = Purchase.objects.filter(user=request.user,
-            recipe=OuterRef('pk'))
-        recipes = recipes.annotate(
-            is_favorite=Exists(favorites_recipes)).annotate(
-            is_purchase=Exists(purchases_recipes))
+        recipes = get_recipes_for_index(recipes, user)
         is_follow = Follow.objects.filter(user=user, author=author).exists()
-
     all_tags = Tag.objects.all()
 
     return render(request, 'recipes/authorRecipe.html', {
         'all_tags': all_tags,
         'author': author,
         'is_follow': is_follow,
-        'purchases_count': purchases_count,
         'recipes': recipes,
     })
 
 
 def recipe_view(request, recipe_id):
+    user = request.user
     recipe = get_object_or_404(
-        Recipe.objects.select_related('author'),
+        Recipe,
         id=recipe_id,
     )
+    author = recipe.author
+    is_favorite = is_follow = is_purchase = False
+    if user.is_authenticated:
+        is_favorite = Favorite.objects.filter(
+            user=user, recipe=recipe).exists()
+        is_follow = Follow.objects.filter(
+            user=user, author=author).exists()
+        is_purchase = Purchase.objects.filter(
+            user=user, recipe=recipe).exists()
+
     ingredients = recipe.ingredients_count.annotate(
         title=F('ingredient__title'),
         unit=F('ingredient__unit')
     )
     return render(request, 'recipes/singlePage.html', {
+        'author': author,
         'recipe': recipe,
         'ingredients': ingredients,
+        'is_favorite': is_favorite,
+        'is_follow': is_follow,
+        'is_purchase': is_purchase,
     })
